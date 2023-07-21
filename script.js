bd658b0487b17b353b75562b8bcd8937c1183359
ethanenkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2023-07-21 14:33:41 -0500

    Add game() function
    game() function enables a full game of 5 rounds and announces a winner at the end.
    
    Also add html content to display on page

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function playRound(playerSelection, computerSelection) {
   } else if (player === "paper") {
     if (computer === "rock") {
       return "You win! Paper beats Rock";
-    } else if (player === "scissors") {
+    } else if (computer === "scissors") {
       return "You lose! Scissors beats Paper";
     }
   } else if (player === "scissors") {
@@ -38,3 +38,43 @@ function playRound(playerSelection, computerSelection) {
 // const computerSelection = getComputerChoice();
 
 // console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt();
+    let result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
+
+    if (result.substring(0, 1) === "T") {
+      console.log("Player: " + playerScore + ", Computer: " + computerScore);
+      continue;
+    } else if (result.substring(0, 1) === "P") {
+      i--;
+      console.log("Player: " + playerScore + ", Computer: " + computerScore);
+      continue;
+    } else if (result.substring(4, 5) === "w") {
+      playerScore++;
+      console.log("Player: " + playerScore + ", Computer: " + computerScore);
+    } else if (result.substring(4, 5) === "l") {
+      computerScore++;
+      console.log("Player: " + playerScore + ", Computer: " + computerScore);
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log(
+      "Game over! Player wins " + playerScore + " to " + computerScore + "!"
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      "Game over! Computer wins " + computerScore + " to " + playerScore + "!"
+    );
+  } else {
+    console.log(
+      "Game over! Tied " + playerScore + " to " + computerScore + "!"
+    );
+  }
+}
